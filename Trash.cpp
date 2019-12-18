c843fbbae328949bb94ee427646857792874c30b
OmemaA <51672463+OmemaA@users.noreply.github.com>
2019-12-18 18:40:37 +0500

    Delete Trash.cpp

--- Trash.cpp
+++
@@ -1,8 +0,0 @@
-#include "Trash.hpp"
-
-Trash::Trash(SDL_Texture *textureSheet, float xstart, float ystart, float xend, float yend, float scale, double w, float h)
-    : GameObject(textureSheet, xstart, ystart, xend, yend, scale, w, h)
-{
-    std::cout << "Trash constructor called" << std::endl;
-}
-void Trash::Update() {}
