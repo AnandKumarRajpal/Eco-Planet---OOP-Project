7a7d72cef9bc459e5330b16556d9909644722dd3
OmemaA <51672463+OmemaA@users.noreply.github.com>
2019-12-18 18:36:26 +0500

    Delete Fire.cpp

--- Fire.cpp
+++
@@ -1,8 +0,0 @@
-#include "Fire.hpp"
-
-Fire::Fire(SDL_Texture *textureSheet, float xstart, float ystart, float xend, float yend, float scale, double w, float h)
-    : GameObject(textureSheet, xstart, ystart, xend, yend, scale, w, h)
-{
-    std::cout << "Fire constructor called" << std::endl;
-}
-void Fire::Update() {}
